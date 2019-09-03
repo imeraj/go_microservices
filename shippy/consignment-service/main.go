2b614ea90e9268d23b9f4ae5e8e408d9d416051e
Meraj <meraj.enigma@gmail.com>
2019-09-03 13:36:46 +0900

    - refactored code - added mongo db support for storing vessel and consigment - updated docker-compose accordingly
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- shippy/consignment-service/main.go
+++ shippy/consignment-service/main.go
@@ -11,35 +11,64 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/server"
 	"golang.org/x/net/context"
+	"gopkg.in/mgo.v2"
 )
 
 ////////
-type IRepository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
+const (
+	defaultHost = "localhost:27017"
+)
+
+const (
+	dbName                = "shippy"
+	consignmentCollection = "consignments"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) error
+	GetAll() ([]*pb.Consignment, error)
+	Close()
 }
 
-type Repository struct {
-	consignments []*pb.Consignment
+type ConsignmentRepository struct {
+	session *mgo.Session
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) error {
+	return repo.collection().Insert(consignment)
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
+func (repo *ConsignmentRepository) GetAll() ([]*pb.Consignment, error) {
+	var consignments []*pb.Consignment
+	// Find normally takes a query, but as we want everything, we can nil this.
+	// We then bind our consignments variable by passing it as an argument to .All().
+	// That sets consignments to the result of the find query.
+	// There's also a `One()` function for single results.
+	err := repo.collection().Find(nil).All(&consignments)
+	return consignments, err
+}
+
+func (repo *ConsignmentRepository) Close() {
+	repo.session.Close()
+}
+
+func (repo *ConsignmentRepository) collection() *mgo.Collection {
+	return repo.session.DB(dbName).C(consignmentCollection)
 }
 
 ///////
 type Service struct {
-	repo         IRepository
+	session      *mgo.Session
 	vesselClient vesselProto.VesselServiceClient
 }
 
+func (s *Service) GetRepo() Repository {
+	return &ConsignmentRepository{s.session.Clone()}
+}
+
 func (s *Service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	defer s.GetRepo().Close()
+
 	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
 		MaxWeight: req.Weight,
 		Capacity:  int32(len(req.Containers)),
@@ -50,34 +79,40 @@ func (s *Service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
 
 	req.VesselId = vesselResponse.Vessel.Id
-	consignment, err := s.repo.Create(req)
+
+	// Save our consignment
+	err = s.GetRepo().Create(req)
 	if err != nil {
 		return err
 	}
 
 	res.Created = true
-	res.Consignment = consignment
+	res.Consignment = req
 	return nil
 }
 
-func (s *Service) GetConsignments(ctx context.Context, req *pb.GetRequest, resp *pb.Response) error {
-	consignments := s.repo.GetAll()
-
-	if len(consignments) > 0 {
-		resp.Created = true
-		resp.Consignment = consignments[0]
+func (s *Service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	defer s.GetRepo().Close()
+	consignments, err := s.GetRepo().GetAll()
+	if err != nil {
+		return err
 	}
-
-	if len(consignments) > 1 {
-		resp.Consignments = consignments[1:]
-	}
-
+	res.Consignments = consignments
 	return nil
 }
 
 ////////
 func main() {
-	repo := &Repository{}
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
+
+	session, err := CreateSession(host)
+	defer session.Close()
+	if err != nil {
+		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
+	}
 
 	// Create a new service. Optionally include some options here.
 	service := grpc.NewService(
@@ -91,7 +126,7 @@ func main() {
 	vesselClient := vesselProto.NewVesselServiceClient("vessel-service", service.Client())
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(service.Server(), &Service{repo, vesselClient})
+	pb.RegisterShippingServiceHandler(service.Server(), &Service{session, vesselClient})
 
 	// Run the server
 	if err := service.Run(); err != nil {
--- shippy/vessel-service/main.go
+++ shippy/vessel-service/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"os"
 
 	pb "github.com/imeraj/go_microservices/shippy/vessel-service/proto/vessel"
@@ -10,33 +10,71 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/server"
 	"golang.org/x/net/context"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 ////////
-type IRepository interface {
+const (
+	defaultHost = "localhost:27017"
+)
+
+const (
+	dbName           = "shippy"
+	vesselCollection = "vessels"
+)
+
+type Repository interface {
 	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+	Create(vessel *pb.Vessel) error
+	Close()
 }
 
-type Repository struct {
-	vessels []*pb.Vessel
+type VesselRepository struct {
+	session *mgo.Session
 }
 
-func (repo *Repository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
-	for _, vessel := range repo.vessels {
-		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
-			return vessel, nil
-		}
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	var vessel *pb.Vessel
+
+	// Here we define a more complex query than our consignment-service's
+	// GetAll function. Here we're asking for a vessel who's max weight and
+	// capacity are greater than and equal to the given capacity and weight.
+	// We're also using the `One` function here as that's all we want.
+	err := repo.collection().Find(bson.M{
+		"capacity":  bson.M{"$gte": spec.Capacity},
+		"maxweight": bson.M{"$gte": spec.MaxWeight},
+	}).One(&vessel)
+	if err != nil {
+		return nil, err
 	}
-	return nil, errors.New("No vessel found by that spec")
+	return vessel, nil
+}
+
+func (repo *VesselRepository) Create(vessel *pb.Vessel) error {
+	return repo.collection().Insert(vessel)
+}
+
+func (repo *VesselRepository) Close() {
+	repo.session.Close()
+}
+
+func (repo *VesselRepository) collection() *mgo.Collection {
+	return repo.session.DB(dbName).C(vesselCollection)
 }
 
 ///////
 type Service struct {
-	repo IRepository
+	session *mgo.Session
+}
+
+func (s *Service) GetRepo() Repository {
+	return &VesselRepository{s.session.Clone()}
 }
 
 func (s *Service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
-	vessel, err := s.repo.FindAvailable(req)
+	defer s.GetRepo().Close()
+	vessel, err := s.GetRepo().FindAvailable(req)
 	if err != nil {
 		return err
 	}
@@ -45,12 +83,42 @@ func (s *Service) FindAvailable(ctx context.Context, req *pb.Specification, res 
 	return nil
 }
 
-////////
-func main() {
-	vessels := []*pb.Vessel{
-		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+func (s *Service) Create(ctx context.Context, req *pb.Vessel, res *pb.Response) error {
+	defer s.GetRepo().Close()
+	if err := s.GetRepo().Create(req); err != nil {
+		return err
 	}
-	repo := &Repository{vessels}
+	res.Vessel = req
+	res.Created = true
+	return nil
+}
+
+////////
+func createDummyData(repo Repository) {
+	defer repo.Close()
+	vessels := []*pb.Vessel{
+		{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
+	}
+	for _, v := range vessels {
+		repo.Create(v)
+	}
+}
+
+func main() {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
+
+	session, err := CreateSession(host)
+	defer session.Close()
+
+	if err != nil {
+		log.Fatalf("Error connecting to datastore: %v", err)
+	}
+
+	repo := &VesselRepository{session.Copy()}
+	createDummyData(repo)
 
 	// Create a new service. Optionally include some options here.
 	service := grpc.NewService(
@@ -62,7 +130,7 @@ func main() {
 	service.Server().Init(server.Address(os.Getenv("MICRO_SERVER_ADDRESS")))
 
 	// Register handler
-	pb.RegisterVesselServiceHandler(service.Server(), &Service{repo})
+	pb.RegisterVesselServiceHandler(service.Server(), &Service{session})
 
 	// Run the server
 	if err := service.Run(); err != nil {
